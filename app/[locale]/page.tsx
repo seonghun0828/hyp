e6bf92f3bc52bf7fec8147a54a9ce973172825f8
jojo <pch468@naver.com>
2026-02-03 22:33:51 +0900

    Feat: resize example images in homepage

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -293,7 +293,7 @@ export default function HomePage() {
 
         {/* Example Section - Expanded for better images */}
         <div className="w-full px-4 md:px-8 lg:px-12 xl:px-16">
-          <div className="max-w-6xl mx-auto">
+          <div className="max-w-3xl mx-auto">
             <ExampleSection />
           </div>
         </div>
